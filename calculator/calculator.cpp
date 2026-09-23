a6d1dae89801f03e9512b584c38528c958c433f4
pepes-al <chaikomaro23@gmail.com>
2026-09-23 15:37:16 +0300

    Implement arithmetic operations and memory features
    Added functionality for basic arithmetic operations and memory management in the calculator.

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -1,2 +1,108 @@
-// В этом файле определения функций.
+#include "calculator.h"
+#include <iostream>
+#include <string>
+#include <cmath>
+
+bool ReadNumber(Number& n) {
+    if (!(std::cin >> n)) {
+        std::cerr << "Error: Numeric operand expected";
+        return false;
+    }
+
+    return true;
+}
+
+bool RunCalculatorCycle() {
+
+    std::string operand;
+    Number number;
+    Number memory;
+    bool is_memory_empty = true;
+
+    if (!ReadNumber(number)) {
+    return false;
+    }
+
+    while (std::cin >> operand) {
+
+        Number num = 0;
+
+        if (operand == "+") {
+            if (!ReadNumber(num)) {
+            return false;
+            }
+            number += num;
+        }
+
+        else if (operand == "-") {
+            if (!ReadNumber(num)) {
+            return false;
+            }
+            number -= num;
+        }
+
+        else if (operand == "*") {
+            if (!ReadNumber(num)) {
+            return false;
+            }
+            number *= num;
+        }
+
+        else if (operand == "/") {
+            if (!ReadNumber(num)) {
+            return false;
+            }
+            number /= num;
+        }
+
+        else if (operand == "**") {
+            if (!ReadNumber(num)) {
+            return false;
+            }
+            number = pow(number, num);
+        }
+
+        else if (operand == "q") {
+            return true;
+        }
+
+        else if (operand == "=") {
+            std::cout << number << std::endl;
+        }
+
+        else if (operand == ":") {
+            if (!ReadNumber(num)) {
+            return false;
+            }
+            number = num;
+        }
+
+        else if (operand == "c") {
+            number = 0;
+        }
+
+        else if (operand == "s") {
+            is_memory_empty = false;    
+            memory = number;
+        }
+
+        else if (operand == "l") {
+            if (!is_memory_empty) {
+                number = memory;
+            }
+            else {
+                std::cerr << "Error: Memory is empty";
+                return false;
+            }
+        }
+
+        else {
+            std::cerr << "Error: Unknown token " << operand;
+            return false;
+        }
+    }
+
+    return false;
+
+}// В этом файле определения функций.
 // За основу возьмите решение предыдущей задачи.
